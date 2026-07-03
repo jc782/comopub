c58b1a1ef9bf76fc93b4080b914e905cee37de1a
Josh Carr <joshcarr3007@gmail.com>
2026-07-03 22:42:23 +0100

    add pubs

--- pubs.js
+++ pubs.js
@@ -15,4 +15,55 @@ const PUBS_DATA = [
     photo: "photos/boathouse.png",
     notes: "Como enjoyed a lovely evening chilling by the river Cam!"
   },
+  {
+    name: "The Victoria",
+    date: "2026-07-01",
+    location: "Holkham Hall",
+    photo: "photos/holkham.jpg",
+    notes: "Como loved his weekend visit to the beach, and his Shepherd's hut sleepover"
+  },
+  {
+    name: "The Red Lion",
+    date: "2026-06-13",
+    location: "Grantchester",
+    photo: "photos/redlion.jpg",
+    notes: "Como went for a walk by the river and a nice pub dinner after. Yummy!"
+  },
+  {
+    name: "The Plough",
+    date: "2026-05-16",
+    location: "Fen Ditton",
+    photo: "photos/plough.jpg",
+    notes: "Como walked all the way from home to the plough, and back!"
+  },
+  {
+    name: "Byron's Bear",
+    date: "2026-04-24",
+    location: "Cambridge",
+    photo: "photos/bear.jpg",
+    notes: "Como 's first beer! Might have started him a little too young."
+  },
+  {
+    name: "The Dophin",
+    date: "2026-05-04",
+    location: "Beer",
+    photo: "photos/doplhin.jpg",
+    notes: "Como 's first trip to the beach! He did get mistaken for a sausage dog though."
+  },
+  {
+    name: "The Haymakers",
+    date: "2026-04-xx",
+    location: "Cambridge",
+    photo: "",
+    notes: "Como's first ever trip to the pub. His local."
+  },
+  {
+    name: "The Plough (Debden)",
+    date: "2026-03-15",
+    location: "Debden",
+    photo: "photos/debden.jpg",
+    notes: "Como visited Debden for Mothering Sunday and had a super fun walk in the countryside"
+  },
+
+
 ];
